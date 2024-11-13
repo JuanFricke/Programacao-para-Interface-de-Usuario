153c8d36783248ca2c263171d2f2cdc4f64a30cf
Orfeu <darkarontg@gmail.com>
2024-11-13 20:39:28 -0300

    go JIT change

--- FinalMission/backend/main.go
+++ FinalMission/backend/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
     router := mux.NewRouter()
 
-    router.HandleFunc("/api/books", getBooks).Methods("GET")
+    router.HandleFunc("/api/livroes/", getBooks).Methods("GET")
     router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
     router.HandleFunc("/api/books", createBook).Methods("POST")
     router.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
