9df4a089c4bc8c3b69dd8034780922248c191f27
Gabriela da Rosa Zanetti <36-gabriela.zanetti@users.noreply.yankee.unijui.edu.br>
2024-11-14 09:27:55 -0300

    refactor: ajuste para aparição da rota ativa

--- FinalMission/frontend/src/app/components/menuLateral/index.tsx
+++ FinalMission/frontend/src/app/components/menuLateral/index.tsx
@@ -1,21 +1,24 @@
 "use client"
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from 'next/navigation'
 import "./style.css";
  
 export default function MenuLateral() {
+    const pathname = usePathname();
+
     return (
         <div className="menu-lateral">
             <div className="nav-item conta-user">
-                <Link href='/conta'>
+                <Link href='/conta' className={pathname === '/perfil' ? 'link-ativo' : ''}>
                     <Image src="/user.png" className="icon-nav icon-user" alt="Perfil" width={20} height={20} />
                 </Link>
             </div>
             <div className="nav-item">
-                <Link href='/' className="link-nav">
+                <Link href='/' className={`${pathname === '/' ? 'link-ativo' : ''} link-nav`}>
                     <Image src="/home.png" alt="Início" className="icon-nav" width={20} height={20} />
                 </Link>
-                <Link href='/projeto' className="link-nav">
+                <Link href='/projeto' className={`${pathname === '/projeto' ? 'link-ativo' : ''} link-nav`}>
                     <Image src="/projetos.png" alt="Projetos" className="icon-nav" width={20} height={20} />
                 </Link>
             </div>
